7a2a7b1c3761d3bfd82f00275ac0de59f75557ab
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-04-21 10:44:10 +0530

    done

---
+++ todo-list-reactjs-main/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./pages/Todo";
+
+const App = () => {
+  return (
+    <div>
+      TODO LIST
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
